088d54fa53954837e24d4d2d2a36093d09f81fab
kaalla <59932396+kaalla@users.noreply.github.com>
2020-01-22 19:56:50 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView){
-    let map1 = new Map({basemap:"topo"});
-    let map2 = new Map({basemap: "streets"})
+
+require(["esri/Map", "esri/views/MapView", 'esri/WebMap'], function(Map, MapView, WebMap){
+    let map1 = new Map({basemap:"satellite"});
+    let map2 = new Map({basemap: "topo"});
+    let map3 = new Map({basemap: "osm"});
+    let map4 = new WebMap({
+        portalItem:{
+            id:'204c475d92b54c03a015db7ae52738e8'
+        }
+    });
+    let przycisk1 = document.getElementById("przycisk1");
+    let przycisk2 = document.getElementById("przycisk2");
+    let przycisk3 = document.getElementById("przycisk3");
+    let przycisk4 = document.getElementById('przycisk4');
+
+
+
     let mapContainer = new MapView({
         container: "mapview",
-        map: map2,
+        map: map4,
         center: [22.56, 51.25],
         zoom: 13
     });
-    let przycisk = document.getElementById("przycisk");
+
+    przycisk4.addEventListener('click', function(){
+        mapContainer.map = map4;
+    })
+    przycisk1.addEventListener('click', function(){
+        mapContainer.map=map1;
+
+    })
+
+    przycisk2.addEventListener('click', function(){
+        mapContainer.map = map2;
+    })
+    przycisk3.addEventListener('click', function(){
+        mapContainer.map = map3
+    })
+    
+
 });
+
+
+
